4040173fe787172971dd474673ad70bbaff82210
yehia67 <yehiatarek67@gmail.com>
2019-07-04 12:38:25 +0200

    done all collison features

--- Board Game/assests/js/battel.js
+++ Board Game/assests/js/battel.js
@@ -88,7 +88,7 @@ function move(worrior) {
             getCastleParent = worriorOneFight.checkCastle(blockplace)
             if (getCastleParent) {
                 blockplace = blockId;
-                console.log("trueee" + blockplace);
+                console.log("***************************************************************trueee" + blockplace);
             }
 
             $(".hint").off('click');
@@ -96,7 +96,7 @@ function move(worrior) {
             $("#worrior-1").remove();
             worrior = createWorrior(1, "One", blockplace); //worrior One
             turn = !turn;
-            holdOne = blockplace;
+            holdOne = getBlockPlace(blockId);
             //console.log("worrior One turn");
             worriorTurn(worriorTwo, '#' + holdeTow);
 
