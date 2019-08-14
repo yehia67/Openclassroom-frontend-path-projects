94934fd0a3777486c824882147017d0d906ea991
yehia67 <yehiatarek67@gmail.com>
2019-08-14 21:30:24 +0200

    done making navbar and footer, start modifying UI to be better ex:add title and start making add reviews feature

--- restaurant/src/components/Footer.js
+++ restaurant/src/components/Footer.js
@@ -1,9 +1,17 @@
 import React from 'react';
-
+import logo from '../logo2.png'
 class Footer extends React.Component {
   render() {
     return (
-<div></div>
+      <footer className="page-footer font-small mt-5" style={{backgroundColor: "#666666"}}>
+
+    <div className="footer-copyright text-center py-3">© 2019 Copyright:
+      <a href="#"> YEHIA TAREK</a>
+    </div>
+
+
+  </footer>
+
     );
   }
 }
--- restaurant/src/components/Header.js
+++ restaurant/src/components/Header.js
@@ -1,11 +1,15 @@
 import React from 'react';
-
+import logo from '../logo2.png';
 class Header extends React.Component {
   render() {
     return (
-        <nav className="navbar navbar-light bg-light">
-        <p className="navbar-brand align-items-center">Restaurent Reviewer</p>
-        </nav>
+
+<nav className="navbar" style={{backgroundColor: "#666666"}}>
+  <a className="navbar-brand" href="#">
+    <img src={logo} width="200" height="30" alt="notfound" />
+  </a>
+</nav>
+
 
     );
   }
